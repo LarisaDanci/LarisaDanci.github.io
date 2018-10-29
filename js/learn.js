cc4c67216daf6039e7f8038c8886699d1bcd44a0
LarisaDanci <43964600+LarisaDanci@users.noreply.github.com>
2018-10-29 20:27:50 +0200

    addstring

--- js/learn.js
+++ js/learn.js
@@ -1,1 +1,17 @@
 console.info(0);
+
+var nume = "Danci";
+var prenume = "Larisa";
+var owner = "Larisa's";
+var msg1 = 'ai zis "Lari"?';
+var msg2 = 'Nu, am zis "Larisa"';
+var remplate = "Glilimele simple";
+
+console.debug("auzi...? dar cum te cheama? -" + prenume);
+
+var skills = ["html", "css", "js"];
+
+prenume = "Lari";
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("cum te cheama? -" + prenume);
