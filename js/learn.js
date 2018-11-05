b0c2eca6d6fff9505b1c9deb08bf4bd5f632c2b3
LarisaDanci <43964600+LarisaDanci@users.noreply.github.com>
2018-11-05 19:55:38 +0200

    add getExractFee

--- js/learn.js
+++ js/learn.js
@@ -36,16 +36,63 @@ console.info("eu am " + myRON + "RON");
 friendRON = friendEUR * cursEUR * 0.99;
 console.info("tu ai " + friendRON + "RON");
 console.info("==== ==== ==== ==== ====");
-function extractFromATM(ammount) {
-  console.info("suma extrasa este: " + ammount);
+
+var ATMFunds = 50000;
+var funds = 5000;
+
+function getExtractFee(ammount) {
   var comision = ammount * 0.01;
   if (comision < 2.5) {
     comision = 2.5;
     console.warn("comision minim aplicat");
   }
+  return comision;
+}
+
+function extractFromATM(ammount) {
+  console.info("suma extrasa este: " + ammount);
+  var comision = getExtractFee(ammount);
+  var totalExtract = ammount + comision;
+
+  if (totalExtract > funds) {
+    console.error("insufficient funds");
+    return;
+  }
+  if (ATMFunds < totalExtract) {
+    console.error("ATM insufficient funds");
+  }
+
+  funds = funds - totalExtract;
+
   console.info("comision aplicat: " + comision);
+  console.info("sold curent: " + funds);
   console.info("==== ==== ==== ==== ====");
 }
 
-extractFromATM(1000);
+extractFromATM(1005); //nu e multiplu de 10
+extractFromATM(100000); //solduri insuficiente
+extractFromATM(3000);
+extractFromATM(3000);
 extractFromATM(100);
+
+var plataTotala = 97;
+var sumaPrimita = 1000;
+var restPlata = 0;
+
+console.info("suma intreaga: " + plataTotala);
+console.info("total cash: " + sumaPrimita);
+restPlata = sumaPrimita - plataTotala;
+console.info("rest: " + restPlata);
+
+var pretInitial = 4000;
+var procentReducere = 20;
+var procent = 100;
+var reducere = 0;
+var pretFinal = 0;
+
+reducere = (procentReducere / procent) * pretInitial;
+pretFinal = pretInitial - reducere;
+
+console.info("pretul initial al produsului este: " + pretInitial);
+console.info("reducerea este: " + reducere);
+console.info("pretul final este: " + pretFinal);
