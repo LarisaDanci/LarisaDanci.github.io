3db2998c7f7b433485e53d323eacb3dae34c55cf
LarisaDanci <larisaadancii@gmail.com>
2018-11-19 20:04:31 +0200

    add education

--- src/index.js
+++ src/index.js
@@ -10,20 +10,32 @@ function show(id) {
 
 function showHomePage() {
   hide("skills-page");
+  hide("education-page");
   hide("languages-page");
   show("home-page");
 }
 function showSkillsPage() {
   hide("home-page");
+  hide("education-page");
   hide("languages-page");
   show("skills-page");
 }
+function showEducationPage() {
+  hide("home-page");
+  hide("skills-page");
+  hide("languages-page");
+  show("education-page");
+}
+
+
 function showLanguagesPage() {
   hide("home-page");
   hide("skills-page");
+  hide("education-page");
   show("languages-page");
 }
 
 $("home-menu").onclick = showHomePage;
 $("skills-menu").onclick = showSkillsPage;
+$("education-menu").onclick = showEducationPage;
 $("languages-menu").onclick = showLanguagesPage;
